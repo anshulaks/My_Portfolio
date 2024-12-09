e7eb03c837a378403009f5d14a6bf940f468f816
AnshulKS <github.com/anshulaks>
2024-12-09 00:01:49 +0000

    new

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -9,7 +9,6 @@ import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bits from "../../Assets/Projects/bit.jpg";
 
-
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -29,7 +28,6 @@ function Projects() {
               title="Chatify"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with MERN stack and Socket-IO. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
               ghLink="https://github.com/anshulaks/chatapp"
-             
             />
           </Col>
           <Col md={4} className="project-card">
@@ -39,11 +37,9 @@ function Projects() {
               title="SQL-Test-Generator"
               description="My personal blog page built with python on the Streamlit framework which takes in the schema from the user and gives out the questions and solutions accordingly."
               ghLink="https://github.com/anshulaks/SQL"
-              // Add inline styling for image size
-              imgStyle={{ width: "100%", height: "auto", objectFit: "cover" }} 
+              imgStyle={{ width: "100%", height: "auto", objectFit: "cover" }} // Add inline styling for image size
             />
           </Col>
-
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={editor}
@@ -51,35 +47,25 @@ function Projects() {
               title="Book Store"
               description="Book store Full Stack Web Application using MongoDB, Express Js, ReactJs with UI design using Talwind CSS and NodeJs."
               ghLink="https://github.com/anshulaks/bookStoreApp"
-                          
             />
           </Col>
-
-          
-
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={suicide}
               isBlog={false}
               title="Crop recommendation"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
+              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace and thus helping in suicide prevention."
               ghLink="https://github.com/anshulaks/Crop-Recommendation.git"
-              
             />
           </Col>
-
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={emotion}
               isBlog={false}
               title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
+              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier successfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
+              Then used Open-CV to detect the face in an image and then pass the face to the classifier to predict the emotion of a person."
               ghLink="https://github.com/anshulaks"
-<<<<<<< HEAD
-=======
-              
->>>>>>> 8d9b0fa93285449514ed75a90dc169edffd10b58
             />
           </Col>
         </Row>
